03e8e9c2a943d1128b0e72744165bc39c57621b7
Lokesh <lokeshsingh7695@gmail.com>
2023-08-17 08:39:16 +0530

    *args and **kwargs

--- args_kwargs.py
+++ args_kwargs.py
@@ -56,3 +56,15 @@ dict = {
 }
 
 print_student(s,*stu,**dict)
+
+
+
+# Clear The concept of kwargs
+def intro(**data):
+    print("\nData type of argument:",type(data))
+
+    for key, value in data.items():
+        print("{} is {}".format(key,value))
+
+intro(Firstname="Sita", Lastname="Sharma", Age=22, Phone=1234567890)
+intro(Firstname="John", Lastname="Wood", Email="johnwood@nomail.com", Country="Wakanda", Age=25, Phone=9876543210)
