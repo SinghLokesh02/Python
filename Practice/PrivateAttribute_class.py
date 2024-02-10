55d796fc9f426aea2913e44aefe13b9c69a14dc0
Lokesh <lokeshsingh7695@gmail.com>
2024-02-10 08:17:03 +0530

    Added how to access Private attribute from outside the class

--- Practice/PrivateAttribute_class.py
+++ Practice/PrivateAttribute_class.py
@@ -1,23 +1,26 @@
 # Creating a Class
 
+
 class Student:
-    name = "Lokesh Singh" # Public Attribute
+    name = "Lokesh Singh"  # Public Attribute
     __id = 1234  # Private Attributes
-    
+
     # Method for Printing Private Attribute
     def Print_Id(self):
         print(f"The Id of student is : {self.__id}")
-    
-    
+
+
 # Creating Object of Class
 
 lokesh = Student()
 
-print(lokesh.name) # Public Attribute can be accessed directly from outside class
+print(lokesh.name)  # Public Attribute can be accessed directly from outside class
 
 # Private Attribute cannot be accessed directly from outside class
-print(lokesh.__id)# This will raise an AttributeError
+# print(lokesh.__id) This will raise an AttributeError
 
 # Accessing private attribute using getter method
 lokesh.Print_Id()
-    
+
+# Accessing private attribute using class name
+print("Printing the Private Member of the class : ", lokesh._Student__id)  # This is called name mangling
