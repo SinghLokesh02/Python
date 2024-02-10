1821b5f6616cc3afa5643f84aa85154d4c7bb2f0
Lokesh <lokeshsingh7695@gmail.com>
2024-02-10 06:33:43 +0530

    Create Private Attribute in Python

---
+++ Practice/PrivateAttribute_class.py
@@ -0,0 +1,23 @@
+# Creating a Class
+
+class Student:
+    name = "Lokesh Singh" # Public Attribute
+    __id = 1234  # Private Attributes
+    
+    # Method for Printing Private Attribute
+    def Print_Id(self):
+        print(f"The Id of student is : {self.__id}")
+    
+    
+# Creating Object of Class
+
+lokesh = Student()
+
+print(lokesh.name) # Public Attribute can be accessed directly from outside class
+
+# Private Attribute cannot be accessed directly from outside class
+print(lokesh.__id)# This will raise an AttributeError
+
+# Accessing private attribute using getter method
+lokesh.Print_Id()
+    
