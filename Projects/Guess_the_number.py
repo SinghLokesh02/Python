41a921dc79872f3a1b7d510701be7b09f4bd3f0a
Lokesh <lokeshsingh7695@gmail.com>
2024-02-12 11:10:33 +0530

    Added some formatting in this Project

--- Projects/Guess_the_number.py
+++ Projects/Guess_the_number.py
@@ -1,5 +1,9 @@
 # Guess the Number Game
 import random
+import time
+
+print("\n\nWelcome to the Guess the Number Game")
+time.sleep(2)
 
 # Generating a random Number between 1 - 100
 guess = round(random.random() * 100)
@@ -7,11 +11,11 @@ guess = round(random.random() * 100)
 data = 0
 count = 0
 while data != guess:
-    data = int(input("Enter Your Number : "))
+    data = int(input("\nEnter Your Number between 1 - 100 : "))
     if guess < data:
         print("You guess is higher")
     elif guess > data:
         print("Your guess is lower")
     count += 1
 
-print(f"You Guessed it right ✅✅ in {count} Attempt")
+print(f"\nYou Guessed it right ✅✅ in {count} Attempt\n")
