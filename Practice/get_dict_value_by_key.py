b41e833119248e84c74d1935d87783c7c124571e
Lokesh <lokeshsingh7695@gmail.com>
2024-02-13 06:05:15 +0530

    Added some more Methods to Get Dict Value by Key

--- Practice/get_dict_value_by_key.py
+++ Practice/get_dict_value_by_key.py
@@ -32,3 +32,28 @@ result = my_dict.get("name")
 
 # Printing the result
 print(f"The value corresponding to key(name) is {result}")
+
+
+# Method - 3 Using Loops
+
+# Creating a Dictionary
+my_dict = {
+    "name" : "Lokesh Singh",
+    "age" : 21,
+    "Roll" : 25,
+    "Role" : "Software Trainer"
+}
+
+for key in my_dict:
+    if key == "name":
+        print(f"The value key {key} is : {my_dict[key]}")
+
+
+
+# Method - 4 Using List Comprehension
+
+# Creating a Dictionary
+my_dict = {"name": "Lokesh Singh", "age": 21, "Roll": 25, "Role": "Software Trainer"}
+key_item = "name"
+result = [my_dict[key] for key in my_dict if key == key_item]
+print(f"The value key {key_item} is : {result}")
