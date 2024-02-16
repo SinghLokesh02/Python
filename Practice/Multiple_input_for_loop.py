e78ff9d041ef522da61c26ec971b7b7fc6b47d34
Lokesh <lokeshsingh7695@gmail.com>
2024-02-16 06:28:20 +0530

    Various Ways to take input

--- Practice/Multiple_input_for_loop.py
+++ Practice/Multiple_input_for_loop.py
@@ -1,21 +1,7 @@
 # How To Take Multiple Inputs In Python Using For Loop
 
-# Create a data type primitive data first
-data1 = data2 = data3 = data4 = data5 = ""
-
-# Run the for loop upto the number of variable you have
-for i in range(5):
-    globals()[f"data{i+1}"] = input(f"Enter data {i+1} : ")
-
-print(data1)
-print(data2)
-print(data3)
-print(data4)
-print(data5)
-
 # Defining the number of data you want to take input
 size = 6
-
 
 # Creating an Empty list
 my_list = []
@@ -28,9 +14,41 @@ for i in range(size):
 # Printing the list
 for data in my_list:
     print(data,end=' ')
-    
-    
 
 
+#  Combining inputs with input().split():
+
+store = input("Enter multiple input separated by spaces : ").split()
+ans = [data for data in store]
+print("The values are : ",ans)
+
+
+
+# Method - 3: Using Dictionary to store the data
+
+# Size of Dictionary
+size = 5
+my_Dict = {}
+
+for i in range(size):
+    key = input(f"Enter the {i+1} key : ")
+    value = input(f"Enter the {i+1} value : ")
+    my_Dict[key] = value
+
+# After taking all the input with the help of loop we are going to print it
+print("\nThe input Values are")
+for key,values in my_Dict.items():
+    print(f"{key} : {values}")
+
+
+
+# Method - 4: Repeated input with single-value storage
+
+number_of_inputs = 5
+# Variable to store all the inputs
+result = 0
+for i in range(number_of_inputs):
+    result += int(input("Enter Any Number : "))
     
-    
+
+print("The total sum of inputted values is : ", result)
