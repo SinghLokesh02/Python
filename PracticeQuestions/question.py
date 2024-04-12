020810c882026a2ceef7482b441ac16dcb0fd624
Lokesh <lokeshsingh7695@gmail.com>
2024-04-12 06:52:56 +0530

    Added Practice questions of List

---
+++ PracticeQuestions/question.py
@@ -0,0 +1,59 @@
+# List in Python - Basic
+
+'''
+Python program to interchange first and last elements in a list
+Python program to swap two elements in a list
+Python – Swap elements in String list
+Python | Ways to find length of list
+Maximum of two numbers in Python
+Minimum of two numbers in Python
+Python | Ways to check if element exists in list
+Different ways to clear a list in Python
+Python | Reversing a List
+Python | Cloning or Copying a list
+Python | Count occurrences of an element in a list
+Python Program to find sum and average of List in Python
+Python | Sum of number digits in List
+Python | Multiply all numbers in the list
+Python program to find smallest number in a list
+Python program to find largest number in a list
+Python program to find second largest number in a list
+Python program to print even numbers in a list
+Python program to print odd numbers in a List
+Python program to print all even numbers in a range
+Python program to print all odd numbers in a range
+Python program to count Even and Odd numbers in a List
+Python program to print positive numbers in a list
+Python program to print negative numbers in a list
+Python program to print all positive numbers in a range
+Python program to print all negative numbers in a range
+Python program to count positive and negative numbers in a list
+Remove multiple elements from a list in Python
+Python | Remove empty tuples from a list
+Python | Program to print duplicates from a list of integers
+
+'''
+
+# List in Python Intermediate
+
+'''
+
+Python Program to count unique values inside a list
+Python – List product excluding duplicates
+Python – Extract elements with Frequency greater than K
+Python – Test if List contains elements in Range
+Python program to check if the list contains three consecutive common numbers in Python
+Python program to find thep Strongest Neighbour
+Python Program to print all Possible Combinations from the three Digits
+Python program to find all the Combinations in the list with the given condition
+Python program to get all unique combinations of two Lists
+Python program to remove all the occurrences of an element from a list
+Python – Remove Consecutive K element records
+Python – Replace index elements with elements in Other List
+Python Program to Retain records with N occurrences of K
+Python Program to Sort the list according to the column using lambda
+
+'''
+
+
+# Function Questions
