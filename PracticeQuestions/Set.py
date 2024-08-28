8dd6aa922c0e450db1a00048e505ad60fa971a81
Lokesh <lokeshsingh7695@gmail.com>
2024-08-28 21:59:47 +0530

    Set practice Questions

---
+++ PracticeQuestions/Set.py
@@ -0,0 +1,43 @@
+# Set
+
+'''
+1. Create a set and add elements to it.
+
+2. Remove an element from a set if it exists; otherwise, do nothing.
+
+3. Check if a specific element is present in a set.
+
+4. Find the length of a set.
+
+5. Given a list of numbers, find all the unique elements using a set.
+
+6. Count the number of unique characters in a string.
+
+7. Given a list of elements, remove all duplicates using a set.
+
+8. Given two sets, find their union.
+
+9. Given two sets, find their intersection.
+
+10. Given two sets, find the difference (elements in set A but not in set B).
+
+11. Given two sets, find the symmetric difference (elements in either set A or set B but not in both).
+
+12. Check if all elements of one set are present in another set.
+
+13. Check if one set is a subset of another.
+
+14. Check if one set is a superset of another.
+
+15. Find the smallest subset of a set that includes a specific element.
+
+16. Compare two sets and determine if they are equal.
+
+17. Convert a set into a sorted list.
+
+18. Given a set, convert it to a list and perform list operations like indexing or slicing.
+
+19. Given a list of elements, use sets to find all duplicates.
+
+20. Given a sentence, find all the unique words using a set.
+'''
