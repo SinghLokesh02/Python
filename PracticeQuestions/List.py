f4a92dd8cf63452368fbed6d08188de54c530ff7
Lokesh <lokeshsingh7695@gmail.com>
2024-08-30 00:04:45 +0530

    Practice Questions of list

---
+++ PracticeQuestions/List.py
@@ -0,0 +1,92 @@
+# List in Python - Basic
+'''
+
+1. Python program to interchange first and last elements in a list.
+
+2. Python program to swap two elements in a list.
+
+3. Python program to swap elements in a string list.
+
+4. Python ways to find the length of a list.
+
+5. Python program to find the maximum of two numbers.
+
+6. Python program to find the minimum of two numbers.
+
+7. Python ways to check if an element exists in a list.
+
+8. Different ways to clear a list in Python.
+
+9. Python program for reversing a list.
+
+10. Python program for cloning or copying a list.
+
+11. Python program to count occurrences of an element in a list.
+
+12. Python program to find the sum and average of a list.
+
+13. Python program to find the sum of digits of numbers in a list.
+
+14. Python program to multiply all numbers in a list.
+
+15. Python program to find the smallest number in a list.
+
+16. Python program to find the largest number in a list.
+
+17. Python program to find the second largest number in a list.
+
+18. Python program to print even numbers in a list.
+
+19. Python program to print odd numbers in a list.
+
+20. Python program to print all even numbers in a range.
+
+21. Python program to print all odd numbers in a range.
+
+22. Python program to count even and odd numbers in a list.
+
+23. Python program to print positive numbers in a list.
+
+24. Python program to print negative numbers in a list.
+
+25. Python program to print all positive numbers in a range.
+
+26. Python program to print all negative numbers in a range.
+
+27. Python program to count positive and negative numbers in a list.
+
+28. Python program to remove multiple elements from a list.
+
+29. Python program to remove empty tuples from a list.
+
+30. Python program to print duplicates from a list of integers.
+ 
+
+'''
+
+# List in Python Intermediate
+
+'''
+
+Python Program to count unique values inside a list
+Python List product excluding duplicates
+Python Extract elements with Frequency greater than K
+Python Test if List contains elements in Range
+Python program to check if the list contains three consecutive common numbers in Python
+Python program to find thep Strongest Neighbour
+Python Program to print all Possible Combinations from the three Digits
+Python program to find all the Combinations in the list with the given condition
+Python program to get all unique combinations of two Lists
+Python program to remove all the occurrences of an element from a list
+Python Remove Consecutive K element records
+Python Replace index elements with elements in Other List
+Python Program to Retain records with N occurrences of K
+Python Program to Sort the list according to the column using lambda
+
+'''
+
+
+
+
+
+
