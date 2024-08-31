b4e7d04cb2f98957c15c72f3ebae1105d38536a5
Lokesh <lokeshsingh7695@gmail.com>
2024-09-01 01:16:29 +0530

    File Handling Questions

---
+++ PracticeQuestions/FileHandling.py
@@ -0,0 +1,18 @@
+'''
+File Handling Practice Questions
+
+1. Write a Python program to read an entire text file.
+2. Write a Python program to count the number of lines in a text file.
+3. Write a Python program to read a text file line by line and print each line.
+4. Write a Python program to read a text file line by line and print each line in reverse order.
+5. Write a Python program to read a text file line by line and print each line containing a specific word.
+6. Write a Python program to read first n lines of a file.
+7. Write a Python program to append text to a file and display the text.
+8. Write a python program to find the longest words in a text file.
+9. Write a Python program to generate 26 text files named A.txt, B.txt, and so on up to Z.txt.
+10. Write a Python program to read a random line from a file.
+11. Write a Python program to Check a file exists or not.
+
+
+
+'''
